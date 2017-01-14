fd36fe42d45987b844bfdf0aef4427bcd458b027
jedrula <andrzej.swaton@gmail.com>
2017-01-14 13:13:18 +0100

    hack for issues with env vars

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-console.log(process.env.PORT, process.env.DIST_PATH);
-//throw 'x';
+// TODO a hack as I had some problems with setting env variables on my raspberry pi
+process.env.PORT = process.env.PORT || 5000;
+process.env.DIST_PATH = process.env.DIST_PATH || '/home/pi/jedrula-app-dist-only/dist';
+
 const FastBootAppServer = require('fastboot-app-server');
 
-let server = new FastBootAppServer({
-  distPath: process.env.DIST_PATH || "../jedrula-app/dist",
+const server = new FastBootAppServer({
+  distPath: process.env.DIST_PATH,
   gzip: true // Optional - Enables gzip compression.
 });
 
